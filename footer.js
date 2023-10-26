4e6ea3b5eab677ac07abc0229f3a38e43f07d8c6
Chris Matsiselts <85128734+G0DF2TH6R@users.noreply.github.com>
2023-10-26 19:17:48 +0300

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,18 @@
+document.write(`
+<footer class="bg-light">
+            <div class="container-fluid">
+                <div class="row">
+                    <ul class="footer-links">
+                        <li>©: 23.10.2023</li>
+                        <li>tel: 55555555</li>
+                        <li><a href="mailto:email@email.com">@: email@email.com</a></li>
+                    </ul>
+                    
+                    <div class="footer-sm">
+                        <a href="#"><img src="img/instagram.png" alt="instagram" /></a>
+                        <a href="#"><img src="img/youtube.png" alt="youtube" /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+`);
